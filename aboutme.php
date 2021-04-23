206838c4217862f8391c9653273ddc4f82c0fda8
nursadrina65 <81969260+nursadrina65@users.noreply.github.com>
2021-04-23 17:29:07 +0700

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Me</title>
+	<link rel="stylesheet" type="text/css" href="aboutme.php">
+</head>
+<body>
+<div class="Latar">
+	<div class="isi">
+		<h3 id="t02">Biodata</h3>
+		<table style="width: 57%" id="t01">
+			<tr>
+				<th rowspan="4"><img src="sadrina65.jpeg" width="150"></th>
+				<th>Nama</th>
+				<td>Nur Sadrina Ghassani</td>
+			</tr>
+			<tr>
+				<th>Kelas</th>
+				<td>XI-4</td>
+			</tr>
+			<tr>
+				<th>NISN</th>
+				<td>19.65.09317</td>
+			</tr>
+			<tr>
+				<th>Angkatan</th>
+				<td>65</td>
+			</tr>
+		</table>
+	</div>
+</div>
+</body>
+</html>
